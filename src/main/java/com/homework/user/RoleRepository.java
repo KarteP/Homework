a2bc7f19335fd3e31281c0e7970e37c44ff55c17
Karte P <k2rte777@hot.ee>
2022-09-18 19:04:26 +0300

    Update RoleRepository.java

--- src/main/java/com/homework/user/RoleRepository.java
+++ src/main/java/com/homework/user/RoleRepository.java
@@ -3,7 +3,7 @@ package com.homework.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     @Query("SELECT r FROM Role r WHERE r.name = ?1")
     Role findByName(String string);
