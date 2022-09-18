92ac2ae296e1c849c985cb23426f57ab2bc5f35f
Karte P <k2rte777@hot.ee>
2022-09-18 16:06:32 +0300

    Add default role

--- src/main/java/com/homework/controller/AppController.java
+++ src/main/java/com/homework/controller/AppController.java
@@ -3,7 +3,6 @@ package com.homework.controller;
 import java.io.IOException;
 import java.util.List;
 
-import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -36,11 +35,6 @@ public class AppController {
 	
 	@GetMapping("/register")
 	public String showRegistrationForm(Model model) {
-		User user = new User();
-
-		Role roleUser = roleRepo.findByName("User");
-        user.addRole(roleUser);
-
 		model.addAttribute("user", new User());
 		
 		return "register";
@@ -51,6 +45,10 @@ public class AppController {
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String encodedPassword = passwordEncoder.encode(user.getPassword());
 		user.setPassword(encodedPassword);
+
+		Role roleUser = roleRepo.findByName("USER");
+        user.addRole(roleUser);
+
 		userRepo.save(user);
 		
 		return "register_success";
@@ -97,6 +95,7 @@ public class AppController {
 	public String editUser(@PathVariable("id") Long id, Model model) {
 		User user = userRepo.getReferenceById(id);
 		List<Role> listRoles = roleRepo.findAll();
+
 		model.addAttribute("user", user);
 		model.addAttribute("listRoles", listRoles);
 		return "user_form";
--- src/main/java/com/homework/user/RoleRepository.java
+++ src/main/java/com/homework/user/RoleRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-
     @Query("SELECT r FROM Role r WHERE r.name = ?1")
     Role findByName(String string);
 
